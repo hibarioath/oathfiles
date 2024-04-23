61152e6f94ab0f31e1a827ce2f3cd3b3e264f167
hibarioath <hibarioath@proton.me>
2024-04-23 21:06:42 +0200

    <3

--- Scripts/Arch.sh
+++ Scripts/Arch.sh
@@ -1,4 +1,5 @@
 # Actualización del sistema y paquetes básicos
+cd ~/
 sudo pacman -Syu
 sudo pacman -S --needed git wget xarchiver cowsay gparted htop neofetch fish links ly man-db ntfs-3g exfatprogs vim plocate sl ufw android-tools iwd openssh
 
@@ -19,24 +20,31 @@ makepkg -si
 cd ..
 
 # i3, polybar, etc.
-sudo pacman -S --needed i3wm polybar capitaine-cursors baobab arandr dunst firefox kitty links galculator kdenlive lxappearance maim xcolor xclip mate-polkit mousepad nemo nemo-fileroller obs-studio pavucontrol picom qbittorrent ristretto gimp syncthing torbrowser-launcher wine python-pywal xfce4-screensaver nitrogen rofi speech-dispatcher unrar unzip vlc xdotool xterm ani-cli
+sudo pacman -S --needed i3 polybar capitaine-cursors baobab arandr keepassxc dunst firefox kitty links galculator kdenlive lxappearance maim xcolor xclip mate-polkit mousepad nemo nemo-fileroller obs-studio pavucontrol picom qbittorrent ristretto gimp syncthing torbrowser-launcher wine python-pywal xfce4-screensaver nitrogen rofi speech-dispatcher unrar unzip vlc xdotool xterm ani-cli
 
 # ly display manager
 sudo systemctl enable ly
 
 # Steam
 echo INSTALACIÓN DE steam
-sudo pacman -S steam
+sudo pacman -S --needed steam
 
 # Creación de varias carpetas
 mkdir Imágenes/ Descargas/ Vídeos/ Música/ Documentos/ ISOs\ SSD/ Wallpaper/ Scripts/ Torrents/
 
+# Mis archivos en GitHub
+git clone https://github.com/hibarioath/oathfiles
+cd oathfiles/
+cp -rfv .config/ .bashrc .vimrc Scripts/ Wallpaper/ ~/
+sudo mkdir /usr/share/themes/ /usr/share/rofi/themes/
+sudo cp -rfv Groove+/ /usr/share/themes/
+sudo cp -rfv nord.rasi /usr/share/rofi/themes/
+sudo cp -rfv Hurmit/ /usr/share/fonts/
+cd ..
+
 # Fuentes
 echo VARIAS FUENTES QUE UTILIZO
 yay -S --needed ttf-linux-libertine ttf-inconsolata ttf-comic-sans ttf-ms-fonts
-cd Descargas/
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hermit.zip
-cd ..
 
 # Xfburn
 echo INSTALACIÓN DE xfburn
@@ -56,7 +64,7 @@ flatpak install pcsx2 duckstation dolphinemu kcolorchooser mediawriter vencord h
 
 # VirtualBox
 echo INSTALACIÓN DE virtualbox, dkms Y linux-lts-headers -necesario para máquinas virtuales-
-sudo pacman -S virtualbox dkms linux-lts-headers
+sudo pacman -S --needed virtualbox dkms linux-lts-headers
 sudo modprober vboxdrv
 
 # Crackling en Pipewire (solución)
@@ -67,9 +75,7 @@ echo 2048 > /proc/sys/dev/hpet/max-user-freq
 git clone https://github.com/hibarioath/oathfiles
 cd oathfiles/
 cp -rfv .config/ .bashrc .vimrc Scripts/ Wallpaper/ ~/
-sudo mkdir /usr/share/themes/ /usr/share/rofi/themes/
+sudo mkdir /usr/share/themes/ /usr/share/rofi/ /usr/share/rofi/themes
 sudo cp -rfv Groove+/ /usr/share/themes/
 sudo cp -rfv nord.rasi /usr/share/rofi/themes/
 cd ..
-
-
