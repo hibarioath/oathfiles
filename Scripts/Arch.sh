fb2646e635ac11c66da7e353f440ec3a70b2e64b
hibarioath <hibarioath@proton.me>
2024-05-05 04:47:21 +0200

    Limpieza

--- Scripts/Arch.sh
+++ Scripts/Arch.sh
@@ -1,7 +1,7 @@
 # Actualización del sistema y paquetes básicos
 cd ~/
 sudo pacman -Syu
-sudo pacman -S --needed git network-manager-applet wget xarchiver gparted htop neofetch fish links man-db ntfs-3g exfatprogs vim plocate ufw android-tools openssh gvfs
+sudo pacman -S --needed git networkmanager network-manager-applet wget xarchiver gparted htop neofetch fish links man-db ntfs-3g exfatprogs vim plocate ufw gvfs
 
 # Instalación de yay-bin
 echo ---INSTALACIÓN DE yay-bin PARA USAR EL AUR---
@@ -13,10 +13,10 @@ cd ..
 
 yay -S --needed blueman
 # i3, polybar, etc.
-sudo pacman -S --needed i3 btop polybar arandr keepassxc dunst firefox xfce4-terminal galculator kdenlive lxappearance maim xcolor audacity ristretto xclip mate-polkit mousepad pcmanfm obs-studio pavucontrol libreoffice-fresh picom qbittorrent gimp syncthing torbrowser-launcher xfce4-screensaver nitrogen rofi speech-dispatcher unrar unzip vlc xdotool
+sudo pacman -S --needed ly i3 btop polybar keepassxc dunst firefox xfce4-terminal galculator lxappearance maim xcolor xclip mate-polkit mousepad pcmanfm obs-studio pavucontrol libreoffice-fresh picom qbittorrent syncthing xfce4-screensaver nitrogen rofi unrar unzip feh mpv xdotool
 
 # ly display manager
-# sudo systemctl enable ly
+sudo systemctl enable ly
 
 sudo pacman -S --needed steam
 
@@ -26,56 +26,25 @@ mkdir Artema/ Saves/ Pictures/ Downloads/ Videos/ Music/ Documents/ Torrents/
 # TLP
 yay -S tlp tlp-rdw
 
-# Mis archivos en GitHub
-cd ~/
-git clone https://github.com/hibarioath/oathfiles
-cd ~/oathfiles/
-cp -rfv .config/ .bashrc .vimrc ~/
-sudo mkdir /usr/share/themes/ /usr/share/rofi/themes/
-sudo cp -rfv nord.rasi /usr/share/rofi/themes/
-cd ..
-
-
 # Dependencias (polybar)
-yay -S yad playerctl pacman-contrib
+# yay -S yad playerctl pacman-contrib
 
 # Fuentes
-yay -S --needed noto-fonts-cjk ttf-noto-emoji-monochrome otf-hermit-nerd ttf-ms-fonts
+yay -S --needed noto-fonts-cjk ttf-noto-emoji-monochrome ttf-dejavu-nerd otf-hermit-nerd ttf-ms-fonts
 
-yay -S --needed cava
-sudo pacman -S --needed xfburn
 yay -S redshifter
 sudo pacman -S --needed iwd
-yay -S --needed ani-cli
 yay -S --needed autotiling-rs-git
-yay -S --needed spotdl
-yay -S --needed gradience-git
-yay -S --needed caffeine-ng
-yay -S --needed automounter
-
-# flatpaks
-# echo INSTALACIÓN DE flatpak Y VARIOS QUE UTILIZO
-# sudo pacman -S --needed flatpak
-#flatpak install pcsx2 duckstation dolphinemu kcolorchooser mediawriter vencord hakuneko flatseal libreoffice gradience ungoogledchromium vscodium
+# yay -S --needed automounter
 
 # VirtualBox
-echo ---INSTALACIÓN DE virtualbox, dkms Y linux-lts-headers NECESARIO PARA MÁQUINAS VIRTUALES---
-sudo pacman -S --needed virtualbox dkms linux-lts-headers
+# echo ---INSTALACIÓN DE virtualbox, dkms Y linux-lts-headers NECESARIO PARA MÁQUINAS VIRTUALES---
+# sudo pacman -S --needed virtualbox dkms linux-lts-headers
 # sudo modprober vboxdrv
 
 # Crackling en Pipewire (solución)
-sudo echo 2048 > /sys/class/rtc/rtc0/max_user_freq
-sudo echo 2048 > /proc/sys/dev/hpet/max-user-freq
-
-# Mis archivos en GitHub
-cd ~/
-git clone https://github.com/hibarioath/oathfiles
-cd oathfiles/
-cp -rfv .config/ .vimrc ~/
-sudo mkdir /usr/share/themes/ /usr/share/rofi/ /usr/share/rofi/themes
-# sudo cp -rfv Groove+/ /usr/share/themes/
-sudo cp -rfv nord.rasi /usr/share/rofi/themes/
-cd ..
+# sudo echo 2048 > /sys/class/rtc/rtc0/max_user_freq
+# sudo echo 2048 > /proc/sys/dev/hpet/max-user-freq
 
 # ufw
 sudo ufw limit 22/tcp
