369c6d83e5185a7bac5570fafa3d9395f38a560e
hibarioath <hibarioath@proton.me>
2024-05-21 02:19:19 +0200

    Fish config

--- Scripts/ConfigsToGit.sh
+++ Scripts/ConfigsToGit.sh
@@ -1,4 +1,4 @@
 cd ~/
 cd .config/
 cp -rfv ~/.vimrc ~/oathfiles 
-cp -rfv libreoffice/ pcmanfm/ alacritty/ fastfetch/ polybar/ i3/ dunst/ picom/ ~/oathfiles/.config/
+cp -rfv fish/ libreoffice/ pcmanfm/ alacritty/ fastfetch/ polybar/ i3/ dunst/ picom/ ~/oathfiles/.config/
