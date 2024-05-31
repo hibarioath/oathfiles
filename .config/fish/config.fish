8cf76a94697956019d7f27bc778a78ede7f299c3
hibarioath <hibarioath@proton.me>
2024-05-31 12:46:17 +0200

    nidea

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias cat='bat'
+# alias cat='bat'
 alias poweroff='sudo poweroff'
-alias reboot='sudo reboot'
+# alias reboot='sudo reboot'
 alias s2ram='s2ram && slock'
 alias powertop='sudo powertop'
 alias eselect='sudo eselect'
--- Scripts/ConfigsToGit.sh
+++ Scripts/ConfigsToGit.sh
@@ -1,4 +1,4 @@
 cd ~/
 cd .config/
 cp -rfv ~/.vimrc ~/oathfiles 
-cp -rfv fish/ libreoffice/ pcmanfm/ alacritty/ fastfetch/ polybar/ i3/ dunst/ picom/ ~/oathfiles/.config/
+cp -rfv sxhkd/ fish/ libreoffice/ pcmanfm/ alacritty/ fastfetch/ polybar/ i3/ dunst/ picom/ ~/oathfiles/.config/
