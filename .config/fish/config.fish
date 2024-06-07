9aecdd407ebf6dfcf26e151c3c957350819c6051
hibarioath <hibarioath@proton.me>
2024-06-08 01:51:11 +0200

    a

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
-# alias cat='bat'
-alias poweroff='sudo poweroff'
-# alias reboot='sudo reboot'
-alias s2ram='s2ram && slock'
-alias powertop='sudo powertop'
-alias eselect='sudo eselect'
-alias equery='sudo equery'
+alias la='ls -la'
 alias emerge='sudo emerge'
+alias eclean='sudo eclean'
+alias equery='sudo equery'
+alias eselect='sudo eselect'
+#alias poweroff='sudo poweroff'
+#alias reboot='sudo reboot'
+alias suspend='~/Artema/Scripts/suspend.sh'
 
+alias flatup='flatpak update'
+alias suck='sudo make clean install'
