991480216023657c82b8e15f206ddacca2469b48
hibarioath <hibarioath@proton.me>
2024-06-27 19:19:09 +0200

    Gentooada0

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-alias la='ls -la'
-alias emerge='sudo emerge'
-alias eclean='sudo eclean'
-alias equery='sudo equery'
-alias eselect='sudo eselect'
-#alias poweroff='sudo poweroff'
-#alias reboot='sudo reboot'
-alias suspend='~/Artema/Scripts/suspend.sh'
+alias nyx='sudo vim /etc/nixos/configuration.nix'
+alias nixch='sudo nixos-rebuild switch'
+alias nixup='sudo nixos-rebuild switch --upgrade'
+alias etctogit='sudo cp -rfv /etc/nixos/* ~/nixoath/'
+alias nixtrash='sudo nix-collect-garbage'
+alias poweroff='sudo nixos-rebuild switch --upgrade; shutdown now'
+
+alias sourcefish='source ~/.config/fish/config.fish'
 
 alias flatup='flatpak update'
 alias suck='sudo make clean install'
